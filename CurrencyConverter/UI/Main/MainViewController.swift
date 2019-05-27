95f1b903f684fea2e4f2d51ff00ba136e966f07d
Sergio Daniel <me@zheref.io>
2019-05-26 19:14:34 -0500

    Add autohidable section for outputs

--- CurrencyConverter/UI/Main/MainViewController.swift
+++ CurrencyConverter/UI/Main/MainViewController.swift
@@ -17,7 +17,33 @@ protocol MainViewControllerProtocol : class {
 
 class MainViewController : FormViewController, MainViewControllerProtocol {
     
+    // MARK: - NESTED TYPES
+    
+    struct K {
+        
+        struct String {
+            static let numberOfDollarBillsSectionTitleComment = "NUMBER OF DOLLAR BILLS"
+            static let numberOfOnesBillsFieldCaptionComment = "#1$ bills"
+            static let numberOfOnesBillsFieldDefaultValue = "0"
+            
+            static let resultsPerCurrencySectionTitleComment = "YOU HAVE..."
+            static let resultsForSecondsCaption = "or..."
+        }
+        
+        struct Tag {
+            static let inputFieldTag = "inputField"
+        }
+        
+    }
+    
     // MARK: - INSTANCE MEMBERS
+    
+    // MARK: Outlets
+    
+    var inputSection: Section!
+    var inputTextRow: TextRow!
+    
+    var outputSection: Section!
     
     // MARK: Stored Properties
     
@@ -43,11 +69,31 @@ class MainViewController : FormViewController, MainViewControllerProtocol {
     // MARK: Exposed Operations -> Protocol Exposed
     
     func buildFormFields() {
-        form +++ Section("NUMBER OF DOLLAR BILLS")
-            <<< TextRow() { row in
-                row.title = "#1$ bills"
-                row.value = "0"
+        let inputSectionName = NSLocalizedString("numberOfDollarBillsSectionTitle", comment: K.String.numberOfDollarBillsSectionTitleComment)
+        let inputFieldCaption = NSLocalizedString("numberOfOnesBillsFieldCaption", comment: K.String.numberOfOnesBillsFieldCaptionComment)
+        
+        let outputSectionName = NSLocalizedString("resultsPerCurrencySectionTitle", comment: K.String.resultsPerCurrencySectionTitleComment)
+        let secondsOutputCaption = NSLocalizedString("resultsForSecondsCaption", comment: K.String.resultsForSecondsCaption)
+        
+        inputTextRow = TextRow(K.Tag.inputFieldTag) { row in
+            row.title = inputFieldCaption
+            row.value = K.String.numberOfOnesBillsFieldDefaultValue
+        }
+        
+        inputSection = Section(inputSectionName)
+            <<< inputTextRow
+        
+        outputSection = Section(outputSectionName)
+        outputSection.hidden = Condition.function([K.Tag.inputFieldTag], { form in
+            guard let inputTextRow = form.rowBy(tag: K.Tag.inputFieldTag) as? TextRow, let value = inputTextRow.value, let intValue = Int(value) else {
+                return true
             }
+            
+            return intValue <= 0
+        })
+        
+        form +++ inputSection
+        form +++ outputSection
     }
 
 }
